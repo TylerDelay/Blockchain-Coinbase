65fdca37e17b1a1d755ae69cfe54f4f7cfdb1165
Tyler DeLay <dela0254@algonquinlive.com>
2022-04-22 20:55:43 -0400

    Added my project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
 
+//chainId for rinkeby is 4
+const supportedChainIds = [4]
+const connectors = {
+  injected: {},
+}
+
+//entire app is wrapped by 3rd web authentication
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
 }
 
 export default MyApp

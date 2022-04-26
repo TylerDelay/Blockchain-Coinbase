7952e60472b357a9b859117fd02979cf904301a3
Tyler DeLay <dela0254@algonquinlive.com>
2022-04-25 20:19:23 -0400

    getCoins finished

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { coins } from "../static/coins";
@@ -6,6 +6,20 @@ import Coin from "./Coin";
 import BalanceChart from "./BalanceChart";
 
 const Portfolio = () => {
+    const [sanityTokens, setSanityTokens] = useState([])
+    useEffect(() => {
+        const getCoins = async () => {
+            try{
+                const coins= await fetch("https://wmv44rtd.api.sanity.io/v1/data/query/production?query=*%5B_type%3D%3D%27coins%27%5D%20%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%0A%7D#")
+                const tempSanityTokens = await coins.json();
+                setSanityTokens(tempSanityTokens.result)
+                console.log(tempSanityTokens.result)
+            }catch (error) {
+                console.log(error)
+            }
+        }
+        getCoins();
+    }, [])
   return (
     <Wrapper>
       <Content>
